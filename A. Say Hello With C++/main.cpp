109c7596f45c7b37e4159acda71c5743021222a3
RehamElaraby <rehamelaraby2272@gmail.com>
2024-07-01 03:08:46 +0200

    2

--- A. Say Hello With C++/main.cpp
+++ A. Say Hello With C++/main.cpp
@@ -7,6 +7,6 @@ int main()
 {
     string name ;
     cin >>  name ;
-    cout << "Hello " << name << endl;
+    cout << "Hello, " << name;
     return 0;
 }
---
+++ B. Basic Data Types/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i ;
+    long long l ;
+    char c ;
+    float f ;
+    double d ;
+    cin >> i >> l >> c >> f >> d ;
+
+    cout <<i<<  endl << l << endl << c  <<  endl << f <<  endl  <<d;
+    return 0;
+}
