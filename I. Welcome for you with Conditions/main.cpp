cc74469650576eeff0f1718e23cf3f715785133f
RehamElaraby <rehamelaraby2272@gmail.com>
2024-07-01 20:01:25 +0200

    9

---
+++ I. Welcome for you with Conditions/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long x , y ;
+    cin >> x >> y ;
+    if(x >= y )
+        cout << "Yes" ;
+    else
+                cout << "No" ;
+
+    return 0;
+}
