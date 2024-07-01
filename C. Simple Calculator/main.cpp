8bb6c7fcb2a76525577ed9c4a036f99dd4b8e29e
RehamElaraby <rehamelaraby2272@gmail.com>
2024-07-01 03:46:32 +0200

    3 , 4

---
+++ C. Simple Calculator/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long x , y ;
+    cin >> x >> y ;
+
+    cout << x << " + " << y <<  " = "  << x+y << endl;
+        cout << x << " * " << y <<  " = "  << x*y << endl;
+    cout << x << " - " << y <<  " = "  << x-y << endl;
+
+
+    return 0;
+}
---
+++ D. Difference/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      long A , B ,C ,D ;
+    cin >> A >> B >> C >> D ;
+           long result = (A*B)-(C*D);
+
+    cout << "Difference = " << result;
+    return 0;
+}
