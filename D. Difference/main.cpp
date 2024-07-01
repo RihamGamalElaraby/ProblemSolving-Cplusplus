bb19f55d8e3bec750e99ae5c6b94962cae7154d5
RehamElaraby <rehamelaraby2272@gmail.com>
2024-07-01 03:49:36 +0200

    Update main.cpp

--- D. Difference/main.cpp
+++ D. Difference/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-      long A , B ,C ,D ;
+   long   long A , B ,C ,D ;
     cin >> A >> B >> C >> D ;
-           long result = (A*B)-(C*D);
+        long   long result = (A*B)-(C*D);
 
     cout << "Difference = " << result;
     return 0;
