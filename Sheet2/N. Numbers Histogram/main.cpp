6856faf08c138b5985de3b811ef78d81412a9e06
RehamElaraby <rehamelaraby2272@gmail.com>
2024-07-06 01:35:32 +0200

    14

--- Sheet2/N. Numbers Histogram/main.cpp
+++ Sheet2/N. Numbers Histogram/main.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    char sign ;
+    cin >> sign ;
+    int n ;
+    cin >> n ;
+    int arr [n];
+    for (int i = 0 ; i < n ; ++i)
+    {
+        cin >> arr[i];
+    }
+    for (int i =0 ; i < n ; ++i)
+    {
+        int temp = arr[i];
+        for (int j = 0 ; j < temp ; ++j)
+        {
+            cout << sign ;
+        }
+         cout << endl;
+    }
     return 0;
 }
