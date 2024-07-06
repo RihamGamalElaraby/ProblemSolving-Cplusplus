a35709e1a09383eb0d6bc5ba004de72e7f5a09b9
RehamElaraby <rehamelaraby2272@gmail.com>
2024-07-06 02:56:55 +0200

    20

---
+++ Sheet2/T. Shape2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n ;
+     cin >> n ;
+     char c = '*' ;
+     for (int i =  0 ; i < n ; ++i)
+     {
+         for (int j = 0 ; j < n-i-1 ; ++j)
+         {
+             cout << " " ;
+         }
+         for (int k = 0 ; k < 2*i+1 ; ++k)
+         {
+             cout << "*" ;
+         }
+         cout << endl;
+     }
+
+    return 0;
+}
